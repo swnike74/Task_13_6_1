9224926f0503ab365bcaa10ce9b59f0a9ec4ae7e
Sergey Vedernikov <swnikeus@outlook.com>
2025-01-28 17:43:51 +0500

    update1

--- Task_13_6_1/Program.cs
+++ Task_13_6_1/Program.cs
@@ -2,9 +2,19 @@
 {
     internal class Program
     {
+        //Наша задача — сравнить производительность вставки в List<T> и LinkedList<T>.Для этого используйте StopWatch.
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // читаем весь файл с рабочего стола в строку текста
+            string text = File.ReadAllText("C:\\Users\\swnik\\Downloads\\cdev_Text.txt");
+
+            // Сохраняем символы-разделители в массив
+            char[] delimiters = new char[] { ' ', '\r', '\n' };
+
+            // разбиваем нашу строку текста, используя ранее перечисленные символы-разделители
+            var words = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            // выводим количество
+            Console.WriteLine(words.Length);
         }
     }
 }
