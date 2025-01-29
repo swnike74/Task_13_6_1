77a33dad976fb4f26cf34bd2863e7161aad22fda
Ведерников Сергей <swnike@mail.ru>
2025-01-29 20:35:10 +0500

    update 3

--- Task_13_6_1/Program.cs
+++ Task_13_6_1/Program.cs
@@ -4,20 +4,15 @@ using System.Diagnostics;
 namespace Task_13_6_1
 {
     internal class Program
-    {
-        //Наша задача — сравнить производительность вставки в List<T> и LinkedList<T>.Для этого используйте StopWatch.
+    {       
         static void Main(string[] args)
         {
-            // читаем весь файл с рабочего стола в строку текста
-            string text = File.ReadAllText("C:\\Users\\swnik\\Downloads\\cdev_Text.txt");
+            string text = File.ReadAllText("C:\\Users\\swnike\\Downloads\\cdev_Text.txt");
 
-            // Сохраняем символы-разделители в массив
             char[] delimiters = new char[] { ' ', ',', '.', '-', '\r', '\n' };
-
-            // разбиваем нашу строку текста, используя ранее перечисленные символы-разделители
             var words = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
             // выводим количество
-            Console.WriteLine(words.Length);
+            Console.WriteLine($"Кол-во слов в массиве {words.Length}");
 
             var booklist = new List<string>();
             var linkedbooklist = new LinkedList<string>();
@@ -27,8 +22,7 @@ namespace Task_13_6_1
                 booklist.Add(word);
                 linkedbooklist.AddLast(word);
             }
-            Console.WriteLine(booklist.Count);
-            Console.WriteLine();
+            Console.WriteLine($"Кол-во слов в списке {booklist.Count}");
 
             Stopwatch stopWatch_tick = new Stopwatch();
             stopWatch_tick.Start();
@@ -38,10 +32,12 @@ namespace Task_13_6_1
                 linkedbooklist.AddAfter(findedword, "Крыса");
 
             stopWatch_tick.Stop();
-            Console.WriteLine(stopWatch_tick.Elapsed.TotalMilliseconds);
+            Console.WriteLine($"Время ввода элемента в связанный список составило { stopWatch_tick.Elapsed.TotalMilliseconds} мс");
 
+            stopWatch_tick.Start();
             booklist.Insert(21, "Лошадь");
-
+            stopWatch_tick.Stop();
+            Console.WriteLine($"Время ввода элемента в обычный список составило {stopWatch_tick.Elapsed.TotalMilliseconds} мс");
         }
     }
 }
